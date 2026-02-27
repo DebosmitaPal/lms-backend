7fa9984ea9a1b9ec12204dcdcbb0597c5a00a0aa
DebosmitaPal <debosmitapal429@gmail.com>
2026-02-27 11:39:24 +0530

    added

--- index.js
+++ index.js
@@ -1,16 +1,44 @@
+// import express from "express";
+// import dotenv from "dotenv";
+// import dbConnect from "./config/db.js";
+// import userRouter from "./routers/userRoute.js";
+// import cors from "cors";
+// const app = express();
+// app.use(express.json());
+// app.use(cors());
+// dotenv.config()
+// const startServer = async () => {
+//   await dbConnect();
+//   app.listen(8080, () => console.log("Server Started"));
+// };
+// startServer()
+
+// app.use("/api/users", userRouter);
+
+
+
 import express from "express";
 import dotenv from "dotenv";
 import dbConnect from "./config/db.js";
 import userRouter from "./routers/userRoute.js";
 import cors from "cors";
+
+dotenv.config();
+
 const app = express();
+
 app.use(express.json());
 app.use(cors());
-dotenv.config()
-const startServer = async () => {
-  await dbConnect();
-  app.listen(8080, () => console.log("Server Started"));
-};
-startServer()
+
+
+await dbConnect();
+
 
 app.use("/api/users", userRouter);
+
+app.get("/", (req, res) => {
+  res.send("LMS Backend is running ");
+});
+
+
+export default app;
